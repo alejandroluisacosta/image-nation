e5d82b27eb2cacfca9de97cad6a4eb36bb3bfcd9
Alejandro <alejandroluisacosta95@gmail.com>
2024-05-23 17:42:14 +0200

    add: favorite images added to local storage

--- src/Features/Favorites/FavoritesSlice.js
+++ src/Features/Favorites/FavoritesSlice.js
@@ -4,15 +4,17 @@ const FavoritesSlice = createSlice({
     name: 'Favorites',
     initialState: {
         status: 'idle',
-        data: [],
+        data: JSON.parse(localStorage.getItem('favorites')) || [],
         error: null
     },
     reducers: {
         addFavorite: (state, action) => {
             state.data.push(action.payload);
+            localStorage.setItem('favorites', JSON.stringify(state.data));
         },
         removeFavorite: (state, action) => {
             state.data = state.data.filter(image => image.id !== action.payload.id);
+            localStorage.setItem('favorites', JSON.stringify(state.data));
         }
     }
 })
