c21c232cea1d71a9d3d9a95ae7d96dd472e26c66
Alejandro <alejandroluisacosta95@gmail.com>
2024-05-23 10:00:49 +0200

    add: favorites header section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
 import './Navbar.css';
+import logoWhite from '/Users/alejandroacosta/Desktop/image-nation/src/assets/ImageNationLogoWhite.png';
+import logoBlack from '/Users/alejandroacosta/Desktop/image-nation/src/assets/ImageNationLogoBlack.png'
+
 const Navbar = ({ className }) => {
     return (
         <nav className={className}>
-            <img src="src/assets/ImageNationLogoWhite.png" alt="Companie's Logo"/>
+            { className === 'header__navbar navbar' ?
+            <>
+            <img src={logoWhite} alt="Companie's Logo"/>
             <Link to="favorites"><button className="navbar__button">Favorites</button></Link>
+            </>
+            :
+            <>
+            <img src={logoBlack} alt="Companie's Logo"/>
+            <Link to="/"><button className="navbar__button--favorites">Home</button></Link>
+            </>
+            }
         </nav>
     )
 }
--- src/Pages/FavoritesPage/FavoritesPage.jsx
+++ src/Pages/FavoritesPage/FavoritesPage.jsx
@@ -1,6 +1,22 @@
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
+import { Input } from "@mui/joy"
+import Navbar from "../../Components/Navbar/Navbar"
+
 const FavoritesPage = () => {
     return (
-        <p>favorites here</p>
+        <>
+        <Navbar className={"navbar navbar--favorites"}/>
+        <h2>Favorites</h2>
+        <Input />
+        <p>Sort by</p>
+        <Select defaultValue="date">
+        <Option value="date">Date</Option>
+        <Option value="width">Width</Option>
+        <Option value="height">Height</Option>
+        <Option value="likes">Likes</Option>
+        </Select>
+        </>
     )
 }
 
