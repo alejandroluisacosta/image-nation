b6f3da4832acbbe61c15b4ec514da3c3e1f4f54c
Alejandro <alejandroluisacosta95@gmail.com>
2024-06-03 11:11:05 +0200

    fix: circular progress component centered

--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import ImageComponent from "../../Components/ImageComponent/ImageComponent";
 import './SearchPage.css';
 import React, { useEffect, useState } from 'react';
